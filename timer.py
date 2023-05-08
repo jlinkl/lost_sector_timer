541ab722d410438d1c82eca86d187d01e6f820b9
James <james.m.linklater@gmail.com>
2023-05-08 14:47:20 +0100

    timer will now increment at every 60th of a second

--- timer.py
+++ timer.py
@@ -1,14 +1,11 @@
 import time
 
+frame = (1.0/60.0)
+
 y = 0
-for x in range(20):
-    y = y + 0.016
-    time.sleep(0.016)
-    print(y)
-    y += 0.017
-    time.sleep(0.017)
-    print(y)
-    y += 0.017
-    time.sleep(0.017)
-    print(y)
+for x in range(60):
+    y += frame
+    time.sleep(frame)
+
+print(y)
 
