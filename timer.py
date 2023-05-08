b3fc883f5d04b65a4b28227e1e88773cd115141f
James <james.m.linklater@gmail.com>
2023-05-08 01:40:43 +0100

    first commit

---
+++ timer.py
@@ -0,0 +1,13 @@
+import time
+y = 0
+for x in range(20):
+    y = y + 0.016
+    time.sleep(0.016)
+    print(y)
+    y += 0.017
+    time.sleep(0.017)
+    print(y)
+    y += 0.017
+    time.sleep(0.017)
+    print(y)
+
