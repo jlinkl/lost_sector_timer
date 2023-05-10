e380aa97a479ca4222ca9d1d48ddd4506ed056d0
James <james.m.linklater@gmail.com>
2023-05-10 15:21:03 +0100

    changed how the screenshot was taken, so there is no need to save it locally anymore

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import pyscreenshot as ImageGrab
-
+import numpy as np
+import cv2
 
 def take_screenshot():
     print("taking screenshot")
-    image_name = "screenshot"
-    screenshot = ImageGrab.grab()
-
-    filepath = f"./screenshots/{image_name}.png"
-
-    screenshot.save(filepath)
-
+    
+    # only looking at a small portion of the screen to reduce processing time
+    screenshot = np.array(ImageGrab.grab(backend="mss", childprocess=False, bbox=(628, 225, 1529, 845)))
     print("screenshot taken")
 
-    return filepath
+    return screenshot
+
+def gamma_correction(screenshot):
+    gamma_increased = np.power(screenshot, 2)
+
+    cv2.imshow('input', screenshot)
+    cv2.imshow('gamma 2', gamma_increased)
+    cv2.waitKey(10000)
+    cv2.destroyAllWindows()
+    
+    return gamma_increased
 
 def main():
-    take_screenshot()
+    screenshot = take_screenshot()
+    gamma_screenshot = gamma_correction(screenshot)
+    
 
 if __name__ == '__main__':
     main()
