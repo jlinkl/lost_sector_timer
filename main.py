50f96d677d3d30db465cbd8c697e34938b09514e
James <james.m.linklater@gmail.com>
2023-05-10 16:13:04 +0100

    wasn't too happy with how often the program was finding non black pixels, so i tweaked the conditions for it a bit.  will need to do more research later

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import time
 def take_screenshot():
     print("taking screenshot")
     time.sleep(2)
-
     # only looking at a small portion of the screen to reduce processing time
     screenshot = np.array(ImageGrab.grab(backend="mss", childprocess=False, bbox=(628, 225, 1529, 845)))
     print("screenshot taken")
@@ -14,8 +13,12 @@ def take_screenshot():
     return screenshot
 
 def gamma_correction(screenshot):
-    gamma_increased = np.power(screenshot, 3)
-
+    gamma_increased = np.power(screenshot, 10)
+    # trying to find if there are non black pixels in the gamma boosted image
+    if np.sum(gamma_increased >= 100) != 0:
+        print(f"found {np.sum(gamma_increased > 0)}")
+    else:
+        print("not found")
     cv2.imshow('input', screenshot)
     cv2.imshow('gamma 2', gamma_increased)
     cv2.waitKey(10000)
