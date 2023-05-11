5578986d741f844a441976fe763c242b0561af68
James <james.m.linklater@gmail.com>
2023-05-11 15:56:11 +0100

    decided to use a different method of finding the time of the run that should be simpler

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pyscreenshot as ImageGrab
 import numpy as np
 import cv2
 import time
+# import timer
 
 def take_screenshot():
     print("taking screenshot")
@@ -16,20 +17,22 @@ def gamma_correction(screenshot):
     gamma_increased = np.power(screenshot, 10)
     # trying to find if there are non black pixels in the gamma boosted image
     if np.sum(gamma_increased >= 100) != 0:
-        print(f"found {np.sum(gamma_increased > 0)}")
+        # print(f"found {np.sum(gamma_increased > 0)}")
+        # by taking note of the time when the starting frame is found, it can be used to find the total time when subtracted from the time of the last frame
+        return time.time()
     else:
-        print("not found")
-    cv2.imshow('input', screenshot)
-    cv2.imshow('gamma 2', gamma_increased)
-    cv2.waitKey(10000)
-    cv2.destroyAllWindows()
-    
-    return gamma_increased
+        # print("not found")
+        return -1
+    # cv2.imshow('input', screenshot)
+    # cv2.imshow('gamma 2', gamma_increased)
+    # cv2.waitKey(10000)
+    # cv2.destroyAllWindows()
 
 def main():
     screenshot = take_screenshot()
-    gamma_screenshot = gamma_correction(screenshot)
-    
+    start_time = gamma_correction(screenshot)
+    while start_time > 0:
+        pass
 
 if __name__ == '__main__':
     main()
